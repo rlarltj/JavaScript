86436363730b592c8a3b6a5e01f6fc9e4659041c
rlarltj <81108344+rlarltj@users.noreply.github.com>
2021-05-05 22:49:37 +0900

    Add files via upload

--- syntex.js
+++ syntex.js
@@ -27,3 +27,39 @@ console.log(globalName);
 // -reduce human mistakes
 
 // 4. Variable types
+// null
+let nothing = null;
+console.log(`value: ${nothing} type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value:{x} type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2); 
+// false 
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2);
+// true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`)
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0));
+// h
+text = 1;
+console.log(`value:${text} , type: ${typeof text}`);
+// number
+text = '7' + 5;
+console.log(`value:${text} , type: ${typeof text}`);
+// String
+
+text = '8' / '2';
+console.log(`value:${text} , type: ${typeof text}`);
+//number
+console.log(text.charAt(0));
+// error
+// 타입스크립트의 필요성
